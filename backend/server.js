3cd113f54a668dc38f7f908aeb5efb79eb486f90
sr1k3sh <sr1k3sh@gmail.com>
2021-12-25 15:50:30 +0545

    test

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { MongoClient } = require('mongodb');
 const bodyParser = require("body-parser");
 const passport = require("passport");
 
+// const path = require('path');
 
 const users = require("./routes/api/users");
 
@@ -18,10 +20,19 @@ app.use(bodyParser.json());
 const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
+
+// const client = new MongoClient(db, { useNewUrlParser: true, useUnifiedTopology: true });
+// console.log(client);
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   console.log(collection)
+//   client.close();
+// });
 mongoose
   .connect(
     db,
-    { useNewUrlParser: true, useUnifiedTopology: true }
+    { useNewUrlParser: true }
   )
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
@@ -32,6 +43,15 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
+
+// //server static assets if in production
+// if(process.env.NODE_ENV == "production"){
+//   app.use(express.static('build'));
+
+//   app.get('*',(req,res)=>{
+//     res.sendFile(path.resolve(__dirname,'build','index.html'));
+//   });
+// }
 
 const port = process.env.PORT || 3000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
