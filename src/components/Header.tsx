e893275622e256ba49128df731ac212afde91751
sr1k3sh <sr1k3sh@gmail.com>
2024-02-04 12:24:19 +0900

    updated header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import { signOut, useSession } from 'next-auth/react';
 import Image from 'next/image';
 
 const Header: React.FC = () => {
-  const router = useRouter();
-  const isActive: (pathname: string) => boolean = (pathname) =>
-    router.pathname === pathname;
 
-const { data: session, status } = useSession();
+  const { data: session, status } = useSession();
+
+  if(status === 'loading') {
+    return <div>Loading...</div>
+  }
+
+  if(!session) {
+    return <></>
+  }
 
   return (
     <nav className='container m-auto px-4 py-8'>
@@ -39,26 +43,26 @@ const { data: session, status } = useSession();
         <ul className='flex flex-row justify-end items-center gap-4'>
           {
             session?.user ?
-            <>
-              <li className='flex flex-row items-center gap-2'>
-                <span className='text-sm'>
-                  Welcome {session?.user?.name}
-                </span>
-                {
-                  session?.user?.image ?
-                  <Image className='aspect-square w-12 h-12 rounded-full' src={session?.user?.image} alt={session?.user?.name} width={50} height={50} />
-                  :
-                  <span className='flex justify-center items-center aspect-square w-12 h-12 rounded-full bg-slate-400'>{session?.user.name?.charAt(0)}</span>
-                }
-              </li>
+              <>
+                <li className='flex flex-row items-center gap-2'>
+                  <span className='text-sm'>
+                    Welcome {session?.user?.name}
+                  </span>
+                  {
+                    session?.user?.image ?
+                      <Image className='aspect-square w-12 h-12 rounded-full' src={session?.user?.image} alt={session?.user?.name} width={50} height={50} />
+                      :
+                      <span className='flex justify-center items-center aspect-square w-12 h-12 rounded-full bg-slate-400'>{session?.user.name?.charAt(0)}</span>
+                  }
+                </li>
+                <li>
+                  <button onClick={() => signOut()}>Logout</button>
+                </li>
+              </>
+              :
               <li>
-                <button onClick={() => signOut()}>Logout</button>
+                <Link href={'/api/auth/signin'}>Login</Link>
               </li>
-            </>
-            :
-            <li>
-              <Link href={'/api/auth/signin'}>Login</Link>
-            </li>
           }
         </ul>
       </div>
